7e7960b0fec839444c5abf92d714d3b08b301699
mariemulemuthoni <mariemuthonimule@gmail.com>
2024-01-30 17:13:36 +0300

    Updated about section content, but the changes are not rendering on the page

---
+++ frontend_react/src/client.js
@@ -0,0 +1,21 @@
+// Connects the Sanity Client to the React App
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+// Define the configuration options for the client
+const clientConfig = {
+    projectId: 'pu813fj9',
+    dataset: 'production',
+    apiVersion: '2022-02-01',
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN
+};
+
+// Create a Sanity client using createClient
+export const client = createClient(clientConfig);
+
+// Create an image builder instance
+const builder = imageUrlBuilder(client);
+
+// Function to build URLs for images
+export const urlFor = (source) => builder.image(source);
